3b96d81b919a270b3be28a6bb2c72c94661e531e
Luktowers <lcs.torss@gmail.com>
2022-07-02 14:56:14 -0300

    New project

---
+++ src/test/java/bank/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
